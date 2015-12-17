2570624cf6cdb3f400a844d8ac7587155d6916ac
GitHubRGI <patrick.doody@rgi-corp.com>
2015-12-17 10:15:29 -0500

    Jitpack to manage GitHub repos via Maven

--- gk-bootstrap.sh
+++ gk-bootstrap.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
 
+sudo add-apt-repository ppa:openjdk-r/ppa
+sudo apt-get update
+sudo apt-get install openjdk-8-jdk
+sudo apt-get install maven
+cd /vagrant/gateway
+mvn clean spring-boot:run
