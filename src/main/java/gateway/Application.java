901abc0a2c8d9eb13d8760371f13ac65c2a796c2
GitHubRGI <patrick.doody@rgi-corp.com>
2016-02-08 09:51:20 -0500

    Fix health check, remove breaking component scan

--- src/main/java/gateway/Application.java
+++ src/main/java/gateway/Application.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"env"})
 public class Application extends SpringBootServletInitializer {
 	
 	@Override
