4f241484ba3b16e95fcdbd42198c080950bcade5
GitHubRGI <patrick.doody@rgi-corp.com>
2016-02-12 07:50:02 -0500

    Comments, formatting

--- src/main/java/gateway/controller/GatewayController.java
+++ src/main/java/gateway/controller/GatewayController.java
@@ -17,9 +17,7 @@ package gateway.controller;
 
 import gateway.auth.AuthConnector;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.UUID;
 
 import javax.annotation.PostConstruct;
@@ -47,22 +45,19 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
-import com.amazonaws.ClientConfiguration;
-import com.amazonaws.Protocol;
-import com.amazonaws.auth.AWSCredentials;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.Bucket;
 import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
-import com.amazonaws.util.StringUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
+/**
+ * Controller that handles the incoming POST requests to the Gateway service.
+ * 
+ * @author Patrick.Doody
+ * 
+ */
 @RestController
 public class GatewayController {
 	/**
@@ -84,7 +79,7 @@ public class GatewayController {
 	private String AMAZONS3_BUCKET_NAME;
 	@Value("${amazons3.domain}")
 	private String AMAZONS3_DOMAIN;
-	
+
 	/**
 	 * Initializing the Kafka Producer on Controller startup.
 	 */
@@ -99,7 +94,8 @@ public class GatewayController {
 	}
 
 	/**
-	 * Executes a Piazza Job
+	 * Handles an incoming Piazza Job request by passing it along from the
+	 * external users to the internal Piazza components.
 	 * 
 	 * @param json
 	 *            The JSON Payload
@@ -113,28 +109,35 @@ public class GatewayController {
 		PiazzaJobRequest request;
 		try {
 			request = JobMessageFactory.parseRequestJson(body);
-		} 
-		catch (Exception exception) {
+		} catch (Exception exception) {
 			return new ErrorResponse(null, "Error Parsing JSON: " + exception.getMessage(), "Gateway");
 		}
 
 		// Authenticate and Authorize the request
 		try {
 			AuthConnector.verifyAuth(request);
-		} 
-		catch (SecurityException securityEx) {
+		} catch (SecurityException securityEx) {
 			return new ErrorResponse(null, "Authentication Error", "Gateway");
 		}
 
-		// Determine if this Job is processed via synchronous REST, or via Kafka message queues.
+		// Determine if this Job is processed via synchronous REST, or via Kafka
+		// message queues.
 		if ((request.jobType instanceof GetJob) || (request.jobType instanceof GetResource)) {
 			return sendRequestToDispatcherViaRest(request);
-		} 
-		else {
+		} else {
 			return sendRequestToDispatcherViaKafka(request, file);
 		}
 	}
-	
+
+	/**
+	 * Forwards the Job request along to internal Piazza components via
+	 * synchronous REST calls. This is for cases where the Job is meant to be
+	 * processed synchronously and the user wants a response immediately for
+	 * their request.
+	 * 
+	 * @param request
+	 * @return
+	 */
 	private PiazzaResponse sendRequestToDispatcherViaRest(PiazzaJobRequest request) {
 		// REST GET request to Dispatcher to fetch the status of the Job ID.
 		// TODO: I would like a way to normalize this.
@@ -154,48 +157,52 @@ public class GatewayController {
 		} catch (RestClientException exception) {
 			return new ErrorResponse(null, "Error connecting to Dispatcher service: " + exception.getMessage(),
 					"Gateway");
-		}		
+		}
 	}
-	
+
+	/**
+	 * Forwards the Job request along to the internal Piazza components via
+	 * Kafka. This is meant for Jobs that will return a job ID, and are
+	 * potentially long-running, and are thus asynchronous.
+	 * 
+	 * @param request
+	 * @param file
+	 * @return
+	 */
 	private PiazzaResponse sendRequestToDispatcherViaKafka(PiazzaJobRequest request, MultipartFile file) {
 		// Create a GUID for this new Job.
 		String jobId = UUID.randomUUID().toString();
-		
-		// If an ingest job, persist the file to the Amazon S3 filesystem
-		if( request.jobType instanceof IngestJob && file != null) {
-			try {		
+
+		// If an Ingest job, persist the file to the Amazon S3 filesystem
+		if (request.jobType instanceof IngestJob && file != null) {
+			try {
 				AmazonS3 client = new AmazonS3Client();
 				client.setEndpoint(AMAZONS3_BUCKET_NAME + AMAZONS3_DOMAIN);
 				client.setRegion(Region.getRegion(Regions.US_EAST_1));
-				client.putObject(AMAZONS3_BUCKET_NAME, file.getOriginalFilename(), file.getInputStream(), new ObjectMetadata());				
-	        } 
-			catch (AmazonServiceException ase) {
-	            System.out.println("Error Message:    " + ase.getMessage());
-	            System.out.println("HTTP Status Code: " + ase.getStatusCode());
-	            System.out.println("AWS Error Code:   " + ase.getErrorCode());
-	            System.out.println("Error Type:       " + ase.getErrorType());
-	            System.out.println("Request ID:       " + ase.getRequestId());			
-				return new ErrorResponse(null, "Caught an AmazonServiceException, which " +
-	            		"means your request made it " +
-	                    "to Amazon S3, but was rejected with an error response" +
-	                    " for some reason.", "Gateway");
-	
-	        } 
-			catch (AmazonClientException ace) {
-	            System.out.println("Error Message: " + ace.getMessage());
-	            ace.printStackTrace();
-				return new ErrorResponse(null, "Caught an AmazonClientException, which " +
-	            		"means the client encountered " +
-	                    "an internal error while trying to " +
-	                    "communicate with S3, " +
-	                    "such as not being able to access the network.", "Gateway");
-	        } 
-			catch (IllegalStateException | IOException e) {
+				client.putObject(AMAZONS3_BUCKET_NAME, file.getOriginalFilename(), file.getInputStream(),
+						new ObjectMetadata());
+			} catch (AmazonServiceException awsServiceException) {
+				System.out.println("Error Message:    " + awsServiceException.getMessage());
+				System.out.println("HTTP Status Code: " + awsServiceException.getStatusCode());
+				System.out.println("AWS Error Code:   " + awsServiceException.getErrorCode());
+				System.out.println("Error Type:       " + awsServiceException.getErrorType());
+				System.out.println("Request ID:       " + awsServiceException.getRequestId());
+				return new ErrorResponse(null, "Caught an AmazonServiceException, which "
+						+ "means your request made it " + "to Amazon S3, but was rejected with an error response"
+						+ " for some reason.", "Gateway");
+
+			} catch (AmazonClientException awsClientException) {
+				System.out.println("Error Message: " + awsClientException.getMessage());
+				awsClientException.printStackTrace();
+				return new ErrorResponse(null, "Caught an AmazonClientException, which "
+						+ "means the client encountered " + "an internal error while trying to "
+						+ "communicate with S3, " + "such as not being able to access the network.", "Gateway");
+			} catch (IllegalStateException | IOException exception) {
 				// TODO Auto-generated catch block
-				e.printStackTrace();
+				exception.printStackTrace();
 			}
 		}
-		
+
 		// Create the Kafka Message for an incoming Job to be created.
 		final ProducerRecord<String, String> message;
 		try {
@@ -206,7 +213,8 @@ public class GatewayController {
 
 		System.out.println("Requesting Job topic " + message.topic() + " with key " + message.key());
 
-		// Fire off a Kafka Message and then wait for a ack response from the kafka broker
+		// Fire off a Kafka Message and then wait for a ack response from the
+		// kafka broker
 		try {
 			producer.send(message).get();
 		} catch (Exception exception) {
@@ -217,6 +225,6 @@ public class GatewayController {
 		}
 
 		// Respond immediately with the new Job GUID
-		return new PiazzaResponse(jobId);		
+		return new PiazzaResponse(jobId);
 	}
 }
