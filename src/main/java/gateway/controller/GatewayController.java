ebcc68678ead62d08d12b06506765d7cd772b549
go0ty <patrick.doody@rgi-corp.com>
2016-03-28 11:32:52 -0400

    slightly more detailed logging

--- src/main/java/gateway/controller/GatewayController.java
+++ src/main/java/gateway/controller/GatewayController.java
@@ -234,7 +234,8 @@ public class GatewayController {
 			PiazzaResponse dispatcherResponse = restTemplate.postForObject(String.format("%s://%s:%s/%s",
 					DISPATCHER_PROTOCOL, DISPATCHER_HOST, DISPATCHER_PORT, endpointString), request.jobType,
 					PiazzaResponse.class);
-			logger.log(String.format("Sent Search Job to Dispatcher REST services"), PiazzaLogger.INFO);
+			logger.log(String.format("Sent Search Job For User %s to Dispatcher REST services", request.apiKey),
+					PiazzaLogger.INFO);
 			// The status code of the response gets swallowed up no matter what
 			// we do. Infer the status code that we should use based on the type
 			// of Response the REST service responds with.
