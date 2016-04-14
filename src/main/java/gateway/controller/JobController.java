f992b6761809146f7c69fcb0176fbaff6a87fe75
go0ty <patrick.doody@rgi-corp.com>
2016-04-14 10:59:13 -0400

    Getting Job Status

--- src/main/java/gateway/controller/JobController.java
+++ src/main/java/gateway/controller/JobController.java
@@ -1,13 +1,22 @@
 package gateway.controller;
 
+import java.security.Principal;
+
+import model.response.ErrorResponse;
 import model.response.PiazzaResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import util.PiazzaLogger;
 
 /**
  * Controller that defines REST end points dealing with Job interactions such as
@@ -15,10 +24,22 @@ import org.springframework.web.bind.annotation.RestController;
  * 
  * @author Patrick.Doody
  *
+ *
  */
 @CrossOrigin
 @RestController
 public class JobController {
+	@Autowired
+	private PiazzaLogger logger;
+	@Value("${dispatcher.host}")
+	private String DISPATCHER_HOST;
+	@Value("${dispatcher.port}")
+	private String DISPATCHER_PORT;
+	@Value("${dispatcher.protocol}")
+	private String DISPATCHER_PROTOCOL;
+
+	private RestTemplate restTemplate = new RestTemplate();
+
 	/**
 	 * Returns the Status of a Job.
 	 * 
@@ -29,11 +50,18 @@ public class JobController {
 	 * @return The response. Contains Job Status, or an appropriate Error.
 	 */
 	@RequestMapping(value = "/job/{jobId}", method = RequestMethod.GET)
-	public ResponseEntity<PiazzaResponse> getJobStatus(@PathVariable(value = "jobId") String jobId) {
+	public ResponseEntity<PiazzaResponse> getJobStatus(@PathVariable(value = "jobId") String jobId, Principal user) {
 		try {
-
+			PiazzaResponse jobStatusResponse = restTemplate.getForObject(String.format("%s://%s:%s/%s/%s",
+					DISPATCHER_PROTOCOL, DISPATCHER_HOST, DISPATCHER_PORT, "job", jobId), PiazzaResponse.class);
+			HttpStatus status = jobStatusResponse instanceof ErrorResponse ? HttpStatus.INTERNAL_SERVER_ERROR
+					: HttpStatus.OK;
+			return new ResponseEntity<PiazzaResponse>(jobStatusResponse, status);
 		} catch (Exception exception) {
-			
+			String error = String.format("Error fetching Job ID %s: %s", jobId, exception.getMessage());
+			logger.log(error, PiazzaLogger.ERROR);
+			return new ResponseEntity<PiazzaResponse>(new ErrorResponse(null, error, "Gateway"),
+					HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
 }
---
+++ src/main/java/gateway/controller/util/GatewayUtil.java
@@ -0,0 +1,12 @@
+package gateway.controller.util;
+
+/**
+ * Utility class that defines common procedures for handling requests,
+ * responses, and brokered end points to internal Piazza components.
+ * 
+ * @author Patrick.Doody
+ *
+ */
+public class GatewayUtil {
+
+}
