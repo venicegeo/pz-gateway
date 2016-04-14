56f12d2326ea7e66e1b0db89e4c384b9b87c58cb
go0ty <patrick.doody@rgi-corp.com>
2016-04-14 08:39:35 -0400

    Utilities, first Endpoint

--- src/main/java/gateway/controller/JobController.java
+++ src/main/java/gateway/controller/JobController.java
@@ -1,6 +1,12 @@
 package gateway.controller;
 
+import model.response.PiazzaResponse;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -13,5 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin
 @RestController
 public class JobController {
+	/**
+	 * Returns the Status of a Job.
+	 * 
+	 * @see https://github.com/venicegeo/venice/wiki/Pz-Gateway#job-status
+	 * 
+	 * @param jobId
+	 *            The ID of the Job.
+	 * @return The response. Contains Job Status, or an appropriate Error.
+	 */
+	@RequestMapping(value = "/job/{jobId}", method = RequestMethod.GET)
+	public ResponseEntity<PiazzaResponse> getJobStatus(@PathVariable(value = "jobId") String jobId) {
+		try {
 
+		} catch (Exception exception) {
+			
+		}
+	}
 }
