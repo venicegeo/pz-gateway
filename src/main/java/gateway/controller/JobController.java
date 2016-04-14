d54e03bd6e9c77e493807921f8323e4aaa43c9e8
go0ty <patrick.doody@rgi-corp.com>
2016-04-14 12:02:39 -0400

    Add delete job endpoint

--- src/main/java/gateway/controller/JobController.java
+++ src/main/java/gateway/controller/JobController.java
@@ -1,10 +1,16 @@
 package gateway.controller;
 
+import gateway.controller.util.GatewayUtil;
+
 import java.security.Principal;
 
+import messaging.job.JobMessageFactory;
+import model.job.type.AbortJob;
+import model.request.PiazzaJobRequest;
 import model.response.ErrorResponse;
 import model.response.PiazzaResponse;
 
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -13,6 +19,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -30,6 +37,8 @@ import util.PiazzaLogger;
 @RestController
 public class JobController {
 	@Autowired
+	private GatewayUtil gatewayUtil;
+	@Autowired
 	private PiazzaLogger logger;
 	@Value("${dispatcher.host}")
 	private String DISPATCHER_HOST;
@@ -43,22 +52,78 @@ public class JobController {
 	/**
 	 * Returns the Status of a Job.
 	 * 
-	 * @see https://github.com/venicegeo/venice/wiki/Pz-Gateway#job-status
+	 * @see http://pz-swagger.stage.geointservices.io/#!/Job/get_job_jobId
 	 * 
 	 * @param jobId
 	 *            The ID of the Job.
+	 * @param user
+	 *            User information
 	 * @return The response. Contains Job Status, or an appropriate Error.
 	 */
 	@RequestMapping(value = "/job/{jobId}", method = RequestMethod.GET)
 	public ResponseEntity<PiazzaResponse> getJobStatus(@PathVariable(value = "jobId") String jobId, Principal user) {
 		try {
+			// Proxy the request to the Dispatcher
 			PiazzaResponse jobStatusResponse = restTemplate.getForObject(String.format("%s://%s:%s/%s/%s",
 					DISPATCHER_PROTOCOL, DISPATCHER_HOST, DISPATCHER_PORT, "job", jobId), PiazzaResponse.class);
 			HttpStatus status = jobStatusResponse instanceof ErrorResponse ? HttpStatus.INTERNAL_SERVER_ERROR
 					: HttpStatus.OK;
+			// Log the request
+			logger.log(
+					String.format("User %s requested Job Status for %s.", gatewayUtil.getPrincipalName(user), jobId),
+					PiazzaLogger.INFO);
+			// Respond
 			return new ResponseEntity<PiazzaResponse>(jobStatusResponse, status);
 		} catch (Exception exception) {
-			String error = String.format("Error fetching Job ID %s: %s", jobId, exception.getMessage());
+			exception.printStackTrace();
+			String error = String.format("Error requesting Job Status for ID %s: %s", jobId, exception.getMessage());
+			logger.log(error, PiazzaLogger.ERROR);
+			return new ResponseEntity<PiazzaResponse>(new ErrorResponse(null, error, "Gateway"),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	/**
+	 * Cancels a running Job, specified by it's Job ID.
+	 * 
+	 * @see http://pz-swagger.stage.geointservices.io/#!/Job/delete_job_jobId
+	 * 
+	 * @param jobId
+	 *            The ID of the Job to delete.
+	 * @param user
+	 *            User information
+	 * @return
+	 */
+	@RequestMapping(value = "/job/{jobId}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> abortJob(@PathVariable(value = "jobId") String jobId,
+			@RequestParam(value = "reason", required = false) String reason, Principal user) {
+		try {
+			// Create the Kafka request object
+			PiazzaJobRequest request = new PiazzaJobRequest();
+			request.userName = gatewayUtil.getPrincipalName(user);
+			request.jobType = new AbortJob(jobId, reason);
+			ProducerRecord<String, String> message = JobMessageFactory.getRequestJobMessage(request, jobId);
+			// Send the Message
+			try {
+				gatewayUtil.sendKafkaMessage(message);
+			} catch (Exception exception) {
+				exception.printStackTrace();
+				// Handle Kafka errors
+				String error = String.format("Error Sending Kafka Message for %s Job %s: %s", "Abort", jobId,
+						exception.getMessage());
+				logger.log(error, PiazzaLogger.ERROR);
+				return new ResponseEntity<PiazzaResponse>(new ErrorResponse(jobId, error, "Gateway"),
+						HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+			// Log the request
+			logger.log(
+					String.format("User %s requested Job Abort for Job ID %s with reason %s",
+							gatewayUtil.getPrincipalName(user), jobId, reason), PiazzaLogger.INFO);
+			// Respond
+			return new ResponseEntity<Void>(HttpStatus.OK);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+			String error = String.format("Error requesting Job Abort for ID %s: %s", jobId, exception.getMessage());
 			logger.log(error, PiazzaLogger.ERROR);
 			return new ResponseEntity<PiazzaResponse>(new ErrorResponse(null, error, "Gateway"),
 					HttpStatus.INTERNAL_SERVER_ERROR);
--- src/main/java/gateway/controller/util/GatewayUtil.java
+++ src/main/java/gateway/controller/util/GatewayUtil.java
@@ -1,5 +1,19 @@
 package gateway.controller.util;
 
+import java.security.Principal;
+
+import javax.annotation.PostConstruct;
+
+import messaging.job.KafkaClientFactory;
+
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import util.PiazzaLogger;
+
 /**
  * Utility class that defines common procedures for handling requests,
  * responses, and brokered end points to internal Piazza components.
@@ -7,6 +21,50 @@ package gateway.controller.util;
  * @author Patrick.Doody
  *
  */
+@Component
 public class GatewayUtil {
+	@Autowired
+	PiazzaLogger logger;
+	@Value("${vcap.services.pz-kafka.credentials.host}")
+	private String KAFKA_ADDRESS;
+	@Value("${kafka.group}")
+	private String KAFKA_GROUP;
 
+	private Producer<String, String> producer;
+
+	/**
+	 * Initializing the Kafka Producer on Controller startup.
+	 */
+	@PostConstruct
+	public void init() {
+		// Kafka Producer.
+		producer = KafkaClientFactory.getProducer(KAFKA_ADDRESS.split(":")[0], KAFKA_ADDRESS.split(":")[1]);
+	}
+
+	/**
+	 * Sends a message to Kafka. This will additionally invoke .get() on the
+	 * message sent, which will block until the acknowledgement from Kafka has
+	 * been received that the message entered the Kafka queue.
+	 * 
+	 * @param message
+	 *            The message to send.
+	 * @throws Exception
+	 *             Any exceptions encountered with the send.
+	 */
+	public void sendKafkaMessage(ProducerRecord<String, String> message) throws Exception {
+		producer.send(message).get();
+	}
+
+	/**
+	 * Safely returns the name of the user who has performed a request to a
+	 * Gateway endpoint.
+	 * 
+	 * @param user
+	 *            The principal
+	 * @return The username. If the request was not authenticated, then that
+	 *         will be returned.
+	 */
+	public String getPrincipalName(Principal user) {
+		return user != null ? user.getName() : "UNAUTHENTICATED";
+	}
 }
