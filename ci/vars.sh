2b96043c5e833fea69c38f79142c757856514a4e
Patrick D <go0ty@users.noreply.github.com>
2017-06-20 09:52:45 -0400

    Update vars.sh

--- ci/vars.sh
+++ ci/vars.sh
@@ -3,3 +3,4 @@
 APP=pz-gateway
 EXT=jar
 ARTIFACT_STORAGE_URL=https://nexus.devops.geointservices.io/content/repositories/Piazza-Group/
+sed -i "s,\${env.ARTIFACT_STORAGE_URL},$ARTIFACT_STORAGE_URL,g" pom.xml
