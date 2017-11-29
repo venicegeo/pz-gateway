4780ac7821f99c2e7cb4144a0069b6b8c1ba7ca9
Patrick D <patrick.doody@rgi-corp.com>
2017-11-29 08:11:41 -0500

    Update Spring Boot

--- src/main/java/gateway/Application.java
+++ src/main/java/gateway/Application.java
@@ -32,7 +32,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
