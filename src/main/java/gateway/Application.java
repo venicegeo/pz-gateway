bacdff948398d21ae1e9bb5e5836884cd9126263
Russell Orf <russellorf@datamentors.com>
2018-01-09 16:02:31 -0500

    orfr: Simplifying the CORS response header configuration

--- src/main/java/gateway/Application.java
+++ src/main/java/gateway/Application.java
@@ -146,7 +146,10 @@ public class Application extends SpringBootServletInitializer {
 			registry.addInterceptor(new HandlerInterceptorAdapter() {
 				@Override
 				public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-					response.addHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+					response.setHeader("Access-Control-Allow-Headers", "authorization, content-type");
+					response.setHeader("Access-Control-Allow-Origin", "*");
+					response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+					response.setHeader("Access-Control-Max-Age", "36000");
 					return true;
 				}
 			});
